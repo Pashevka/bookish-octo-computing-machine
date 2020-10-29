a71b6177133b683554195508b8ec2c7315b59b75
Pavel <Pashevka@gmail.com>
2020-10-29 14:50:10 +0300

    Added adaprive features

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
+  document.getElementsByTagName("body")[0].style.opacity = 1;
   $(".kata-slider").slick({});
+  $(".kata-slider-access").slick({
+    dots: true,
+    arrows: false,
+  });
 });
